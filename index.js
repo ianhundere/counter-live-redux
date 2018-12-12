59ef725946184a92f089ceb25f7364d734dda14a
Ian Hundere <ianfundere@gmail.com>
2018-12-12 11:36:06 -0500

    adds uuid and further counter logic

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const { createStore } = require('redux');
+const uuid = require('uuid/v4');
 
 // #1 write out an example/default version of app state
 
 const defaultState = {
-    count: 0
+    // count: 0
+    counters: [
+        {
+            id: uuid(),
+            count: 0
+        }
+    ]
 };
 
 // #2a describe the ways that state can change
@@ -21,6 +28,24 @@ const ACTION_DEC = {
     type: 'DECREMENT'
 };
 
+// "Action Creators"
+// when you need to configure an action, write a function
+const incrementCounter = id => {
+    return {
+        ...ACTION_INC,
+        id
+    };
+};
+// example: store.dispatch(incrementCounter('abc-123-do'))
+
+const decrementCounter = id => {
+    return {
+        ...ACTION_DEC,
+        id
+    };
+};
+// example: store.dispatch(decrementCounter('abc-123-do'))
+
 // #3 - write a pure function that accepts the current state and an action,
 // then returns the new version state
 
@@ -31,7 +56,21 @@ const counterReducerFunc = (state = defaultState, action) => {
         case ACTION_INC.type:
             // if it's 'INCREMENT', return a new state object with the count + 1
             return {
-                count: state.count + 1
+                // count: state.count + 1
+                // we want to return the array of counters
+                // but we want to modify the one where its id === action.id
+                counters: state.counters.map(oneCounter => {
+                    if (oneCounter.id === action.id) {
+                        // return a new vers of oneCounter
+                        return {
+                            ...oneCounter,
+                            count: oneCounter.count + 1
+                        };
+                    } else {
+                        // these are not the droids i'm lookg for
+                        return oneCounter;
+                    }
+                })
             };
         // break: no need to break, since we're returning
         // if you're not returning, use break to make sure other cases
@@ -53,7 +92,7 @@ const store = createStore(counterReducerFunc);
 // You can subscribe to notifications of any changes to the state
 store.subscribe(() => {
     const theState = store.getState();
-    console.log(`The state is now: ${theState}`);
+    console.log(`The state is now: ${theState.count}`);
 });
 
 module.exports = {
